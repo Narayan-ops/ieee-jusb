f80685734ec5b550bfa77b9c634b5529e2f1a228
Narayan-ops <sharannarayan9@gmail.com>
2026-09-17 16:28:26 +0530

    chore: hide MLAS4 popup

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import AnimatedRoutes from "./Components/AnimatedRoutes";
-import MLAS4Popup from "./Components/MLAS4Popup";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <MLAS4Popup />
         <Navbar />
         <AnimatedRoutes />
         <Footer />
